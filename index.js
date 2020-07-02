d564191c2065bcbc1c63292f626e32e90387f542
Encke Technologies <james@encke.com>
2020-07-01 22:02:54 -0600

    Wrapped

--- index.js
+++ index.js
@@ -7,42 +7,42 @@ if( !NK.files.exists( configFileName ) )  {
 const config = require( "./config" );
 
 if( config.enabled ) {
-NK.start( true, null, null, null, () => NK.db.start( config.mongo.name, config.mongo.host, config.mongo.port, () => {
-  let mySQLConnection = mysql.createConnection( config.mysql );
-  mySQLConnection.connect();
-  let tables = NK.objCopy( config.tables );
-  let loadNextTable = () => {
-    let thisTable = ( ( tables && ( tables.length > 0 ) )? tables.shift(): null );
-    if( thisTable ) {
-      if( thisTable.copyData ) {
-        mySQLConnection.query( ( "SELECT * FROM " + thisTable.name ), ( error, results, fields ) => {
-          console.log(error, results, fields);
-          //for each table, get all rows
-          let loadArray = [];
+  NK.start( true, null, null, null, () => NK.db.start( config.mongo.name, config.mongo.host, config.mongo.port, () => {
+    let mySQLConnection = mysql.createConnection( config.mysql );
+    mySQLConnection.connect();
+    let tables = NK.objCopy( config.tables );
+    let loadNextTable = () => {
+      let thisTable = ( ( tables && ( tables.length > 0 ) )? tables.shift(): null );
+      if( thisTable ) {
+        if( thisTable.copyData ) {
+          mySQLConnection.query( ( "SELECT * FROM " + thisTable.name ), ( error, results, fields ) => {
+            console.log(error, results, fields);
+            //for each table, get all rows
+            let loadArray = [];
 
 
-          let sendToMongo = () => {
-            let sendArray = NK.objCopy( loadArray );
-            if( loadArray.length > thisTable.batchSize )  {
-              sendArray = loadArray.slice( 0, thisTable.batchSize );
-              loadArray = loadArray.slice( thisTable.batchSize );
-            }
-            if( sendArray.length > 0 )  {
-              NK.db.insert( config.mongo.name, thisTable.name, sendArray, () => sendToMongo() );
-            } else {
-              loadNextTable();
-            }
-          };
-          sendToMongo();
-        });
+            let sendToMongo = () => {
+              let sendArray = NK.objCopy( loadArray );
+              if( loadArray.length > thisTable.batchSize )  {
+                sendArray = loadArray.slice( 0, thisTable.batchSize );
+                loadArray = loadArray.slice( thisTable.batchSize );
+              }
+              if( sendArray.length > 0 )  {
+                NK.db.insert( config.mongo.name, thisTable.name, sendArray, () => sendToMongo() );
+              } else {
+                loadNextTable();
+              }
+            };
+            sendToMongo();
+          });
+        } else {
+          loadNextTable();
+        }
       } else {
-        loadNextTable();
+        mySQLConnection.end();
       }
-    } else {
-      mySQLConnection.end();
-    }
-  };
-  loadNextTable();
-}));
+    };
+    loadNextTable();
+  }));
 };
 //add ability for SQLLite and MSSQL
